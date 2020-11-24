b262906d8ae02c203f6beb2f5e4fa502d0d2ed72
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-11-23 21:33:53 -0600

    183/4 problems A and B

---
+++ AtCoder/ABC/183A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define ll long long
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}	
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int x;
+	cin >> x;
+	
+	if (x >= 0) {
+		cout << x << "\n";
+	}
+	else {
+		cout << 0 << "\n";
+	}
+	
+	return 0;
+}
