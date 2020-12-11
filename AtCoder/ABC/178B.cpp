c2e347acf781909d2b1cb502d88bcf110b2201c2
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-10 19:31:19 -0600

    Solution of problem

---
+++ AtCoder/ABC/178B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define ll long long
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}	
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	ll a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	ll ans = -1e18;
+	
+	ans = max(ans, a * c);
+	ans = max(ans, a * d);
+	ans = max(ans, b * c);
+	ans = max(ans, b * d);
+
+	cout << ans << "\n";
+	
+	return 0;
+}
