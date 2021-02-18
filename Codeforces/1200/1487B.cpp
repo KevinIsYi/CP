f772dc505df4258c664f897021180ec578d55084
Kevin <rodriguezkevin95g@gmail.com>
2021-02-17 19:14:52 -0600

    Solution of problem

---
+++ Codeforces/1200/1487B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define ll long long
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}	
+
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	
+	if (n % 2 == 0) {
+		if (k % n == 0) {
+			cout << n << "\n";
+		}
+		else {
+			cout << k % n << "\n";
+		}
+	}
+	else {
+		--k;
+		ll jumps = k / (n / 2);
+		cout << ((k % n) + jumps) % n + 1 << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	
+	while(t--) {
+		solve();
+	}
+	
+	return 0;
+}
