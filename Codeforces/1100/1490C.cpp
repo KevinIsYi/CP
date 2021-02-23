c005f76f7e187097b0d8c5e6269b8397482ab074
Kevin <rodriguezkevin95g@gmail.com>
2021-02-23 10:56:01 -0600

    Solution of problem

---
+++ Codeforces/1100/1490C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define ll long long
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}	
+
+ll n = 1e12;
+set<ll> cubes;
+void pre() {
+	for (ll i = 1 ; i * i * i <= n ; ++i) {
+		cubes.insert(i * i * i);
+	}
+}
+
+void solve() {
+	cin >> n;
+	for (ll i = 1 ; i * i * i <= n ; ++i) {
+		if (cubes.count(n - i * i * i)) {
+			cout << "YES\n";
+			return;
+		}
+	}
+	cout << "NO\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	
+	pre();
+	while(t--) {
+		solve();
+	}
+	
+	return 0;
+}
